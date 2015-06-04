0d06903a380c5529044b645bd7947c9457a950bd
Zhang, Wei <zweiustc@gmail.com>
2015-06-04 10:02:46 +0800

    add random nanme for vms and volumes

--- HA_service_test/HA_Manage/HA_test.sh
+++ HA_service_test/HA_Manage/HA_test.sh
@@ -1,3 +1,5 @@
+#Test the openstack env to make sure every modules is ready
+#author: zhangwei18
 #!/bin/sh
 
 imageid='31f8012c-7623-4b51-96e2-29efb020dd2f'
@@ -7,6 +9,8 @@ do
     #echo `date` ': keystone token-get' >> ha_test.log
     #keystone token-get >> ha_test.log
     #sleep 2
+    vmname='zhangwei'$RANDOM
+    echo "vm name: "$vmname
 
     cmd='keystone tenant-list'
     echo `date` ': ' $cmd >> ha_test.log
@@ -44,13 +48,13 @@ do
     sleep 2
     echo '      ' >>  ha_test.log
 
-    cmd='nova get-vnc-console bd943494-a3e8-45ac-8ea5-dd26b453a1eb novnc'
-    echo `date` ': ' $cmd >> ha_test.log
-    $cmd >> ha_test.log
-    sleep 2
-    echo '      ' >>  ha_test.log
+    #cmd='nova get-vnc-console bd943494-a3e8-45ac-8ea5-dd26b453a1eb novnc'
+    #echo `date` ': ' $cmd >> ha_test.log
+    #$cmd >> ha_test.log
+    #sleep 2
+    #echo '      ' >>  ha_test.log
 
-    cmd="cinder create --image-id $imageid --name zhangweivolume1 20"
+    cmd="cinder create --image-id $imageid --name $vmname 20"
     echo `date` ': ' $cmd >> ha_test.log
     $cmd >> ha_test.log
     sleep 6
@@ -69,9 +73,9 @@ do
     echo '      ' >>  ha_test.log
 
 
-    volumeid=`cinder list | grep zhangweivolume1 | awk  '{print $2}'`
+    volumeid=`cinder list | grep $vmname | awk  '{print $2}'`
 
-    cmd="nova boot --boot-volume $volumeid --flavor 2 --nic net-id=9f07ae46-13e9-46b6-8e67-89b05c061a8a zhangweivolume1_vm1"
+    cmd="nova boot --boot-volume $volumeid --flavor 2 --nic net-id=9f07ae46-13e9-46b6-8e67-89b05c061a8a $vmname"
     echo `date` ': ' $cmd >> ha_test.log
     $cmd >> ha_test.log
     sleep 60
@@ -83,7 +87,13 @@ do
     sleep 2
     echo '      ' >>  ha_test.log
 
-    cmd='nova floating-ip-associate zhangweivolume1_vm1 10.160.60.104'
+    cmd="nova get-vnc-console $vmname novnc"
+    echo `date` ': ' $cmd >> ha_test.log
+    $cmd >> ha_test.log
+    sleep 2
+    echo '      ' >>  ha_test.log
+
+    cmd="nova floating-ip-associate $vmname 10.160.60.104"
     echo `date` ': ' $cmd >> ha_test.log
     $cmd >> ha_test.log
     sleep 2
@@ -101,7 +111,7 @@ do
     sleep 10
     echo '      ' >>  ha_test.log
 
-    cmd='nova delete zhangweivolume1_vm1'
+    cmd="nova delete $vmname"
     echo `date` ': ' $cmd >> ha_test.log
     $cmd >> ha_test.log
     sleep 10
@@ -112,4 +122,11 @@ do
     $cmd >> ha_test.log
     sleep 10
     echo '      ' >>  ha_test.log
+
+    echo "-------------------------------------------------------"
+    echo "----------                               --------------"
+    echo "----------                               --------------"
+    echo "----------    One round arrived the end  --------------"
+    echo "----------                               --------------"
+    echo "-------------------------------------------------------"
 done
